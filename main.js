bd0ac483a15cf179c539e618f39311aec4aea832
Mickey Mazock <mmazock@sacs.k12.in.us>
2026-02-10 10:48:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-console.log("Old World Trade Game loaded");
+const mapImage = document.getElementById("map-image");
 
+mapImage.addEventListener("click", function (event) {
+  const rect = mapImage.getBoundingClientRect();
+
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  console.log(`Map clicked at: X=${Math.round(x)}, Y=${Math.round(y)}`);
+});
